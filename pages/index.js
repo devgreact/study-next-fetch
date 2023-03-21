4492d99e563c1c0cec2bb99c2e0095ac3dba24b2
devgreact <dev.greact@gmail.com>
2023-03-21 23:10:42 +0900

    ### SSR(Sever-Side-Rendering) 테스트 : getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -24,9 +24,10 @@ export default function Home({ cats }) {
 // - 사용자의 요청마다 동적으로 페이지를 생성해 다른 내용을 보여주어야 하는 경우에 사용됩니다.
 // - 그때 그때 보여주는 방식
 export const getServerSideProps = async () => {
-  const res = await fetch(
-    "https://api.thecatapi.com/v1/breeds?api_key=live_Z9dx0VtiK2f0qbMsh1fhE7Z3Sw21vaP79MAhtKChl3XFPpWKvoBDSa6OSqZHYNSJ&limit=10"
-  );
+  // const res = await fetch(
+  //   "https://api.thecatapi.com/v1/breeds?api_key=live_Z9dx0VtiK2f0qbMsh1fhE7Z3Sw21vaP79MAhtKChl3XFPpWKvoBDSa6OSqZHYNSJ&limit=10"
+  // );
+  const res = await fetch(`http://localhost:8080/api/cats`)
   const cats = await res.json();
   return {
     props: {
